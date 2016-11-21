0e704c348d209e276fe83a3ff1ca80fd8b811dd1
Iftekher Sunny <iftekhersunny@hotmail.com>
2016-11-21 11:52:06 +0600

    Added status method to get broadcast status

--- src/Broadcast.php
+++ src/Broadcast.php
@@ -12,6 +12,34 @@ class Broadcast extends ApiResource
     protected $resource = 'broadcasts';
 
     /**
+     * Broadcast status value as Drafted.
+     *
+     * @var string
+     */
+    public static $DRAFTED = 'Drafted';
+
+    /**
+     * Broadcast status value as Delivered.
+     *
+     * @var string
+     */
+    public static $DELIVERED = 'Delivered';
+
+    /**
+     * Broadcast status value as Queued.
+     *
+     * @var string
+     */
+    public static $PENDING = 'Queued';
+
+    /**
+     * Broadcast status value as Processing.
+     *
+     * @var string
+     */
+    public static $PROCESSING = 'Processing';
+
+    /**
      * Send broadcast email.
      *
      * @param int   $id
@@ -24,6 +52,20 @@ class Broadcast extends ApiResource
         return $this->request->post(
             "{$this->getResourceName()}/{$id}/send?api_token={$this->request->getApiKey()}",
             $data
+        );
+    }
+
+    /**
+     * Get filtered broadcasts by the given broadcast status.
+     *
+     * @param string $status
+     *
+     * @return mixed
+     */
+    public function status($status = 'status')
+    {
+        return $this->request->get(
+            "{$this->getResourceName()}/{$status}?api_token={$this->request->getApiKey()}"
         );
     }
 }
