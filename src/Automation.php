f8b60a28316b0a0325c2fe57d2db1ef2cb069623
Iftekher Sunny <iftekhersunny@hotmail.com>
2017-04-06 11:20:52 +0600

    Added automation status changer api

--- src/Automation.php
+++ src/Automation.php
@@ -10,4 +10,18 @@ class Automation extends ApiResource
      * @var string
      */
     protected $resource = 'automations';
+
+    /**
+     * Change automation status by the given ids.
+     *
+     * @param array $ids
+     * @return mixed
+     */
+    public function changeStatus($ids)
+    {
+        return $this->request->post(
+            "{$this->getResourceName()}/status?api_token={$this->request->getApiKey()}",
+            ["ids" => $ids]
+        );
+    }
 }
